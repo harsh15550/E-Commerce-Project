af996b61b1f5f62e7a7b9b7b3b72440fbc174b95
harsh15550 <hp877257@gmailcom>
2025-06-29 14:00:57 +0530

    first commit

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -8,7 +8,7 @@ export const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'e-commerce-project-frontend-bay.vercel.app'],
         methods: ['GET', 'POST']
     }
 });
